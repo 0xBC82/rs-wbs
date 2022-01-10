b29dda83b85f10f12973ed2b0b7ca7d84a7bf223
0xBC82 <0xBC82@protonmail.com>
2022-01-11 08:37:23 +1100

    Add multi-threaded support

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,13 @@
 use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use rs_wbs::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
-        handle_connection(stream.unwrap());
+        pool.execute(|| handle_connection(stream.unwrap()));
     }
 }
 
