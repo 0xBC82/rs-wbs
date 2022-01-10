ef62da25bd5e2c3c7cb1c6149db3d18579b259e2
0xBC82 <0xBC82@protonmail.com>
2022-01-10 20:28:44 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rs-wbs
+
+Rust web server from the [book](https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html).
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        handle_connection(stream.unwrap());
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let _request = String::from_utf8_lossy(&buffer[..]);
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+        let status_line = "HTTP/1.1 200 OK";
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
